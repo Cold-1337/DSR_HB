cdedb4571e30c164c68465a63f49b9e95e19c191
Cold-1337 <leistenschneiderdominic@gmail.com>
2026-03-06 20:03:31 +0100

    feat: upcast support + autorelease workflow

--- src/features/twisted-grace/index.js
+++ src/features/twisted-grace/index.js
@@ -1,5 +1,6 @@
 import { registerChaoticBenediction } from "./spells/chaotic-benediction.js";
 
 export function registerTwistedGrace() {
+  if (!game.settings.get("DSR_HB", "twistedGraceEnabled")) return;
   registerChaoticBenediction();
 }
